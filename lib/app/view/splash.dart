327056fd2ecca04b0026e1b46ef4d8554506e7a3
Wadson Nilton <niiltonsk8@gmail.com>
2025-01-21 10:52:41 -0300

    fix: ajuste de navegação para funcionar em toda a tela da SplashScreen

--- lib/app/view/splash.dart
+++ lib/app/view/splash.dart
@@ -7,49 +7,49 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Image.asset(
-                'assets/images/shape.png',
-                width: 141,
-                height: 129,
-              ),
-            ],
-          ),
-          const SizedBox(height: 79),
-          Image.asset(
-            'assets/images/onboarding-image.png',
-            width: 180,
-            height: 168,
-          ),
-          const SizedBox(height: 79),
-          Text(
-            'Lista de Tarefas',
-            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => TaskListPage(),
-                ),
-              );
-            },
-          ),
-          const SizedBox(height: 21),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 32),
-            child: Text(
-              'A melhor forma de não se esquecer de nada é anotando. Guarde suas tarefas e vá completando aos poucos para aumentar sua produtividade',
-              textAlign: TextAlign.center,
+      body: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => TaskListPage(),
             ),
-          ),
-        ],
+          );
+        },
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  'assets/images/shape.png',
+                  width: 141,
+                  height: 129,
+                ),
+              ],
+            ),
+            const SizedBox(height: 79),
+            Image.asset(
+              'assets/images/onboarding-image.png',
+              width: 180,
+              height: 168,
+            ),
+            const SizedBox(height: 79),
+            Text(
+              'Lista de Tarefas',
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+            ),
+            const SizedBox(height: 21),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              child: Text(
+                'A melhor forma de não se esquecer de nada é anotando. Guarde suas tarefas e vá completando aos poucos para aumentar sua produtividade',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
